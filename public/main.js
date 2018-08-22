a83e7596abcfdc61e37f558c2f232573fb676d9b
Henry Cuffy <hcuffy_5@hotmail.com>
2018-08-22 07:21:00 +0200

    add dots

--- public/main.js
+++ public/main.js
@@ -3,16 +3,32 @@ $(document).ready(function() {
 		'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/cyclist-data.json',
 		function(data) {
 
-			const w = 500
-			const h = 500
-
+			const w = 900
+			const h = 400
+			const padding = 80
 			var dataset = []
-			data.forEach((entry) => {
-				var timeTesz = entry.Time.split(':')
-				var newtime = new Date(Date.UTC(1970, 0, 1, 0, timeTesz[0], timeTesz[1]))
-				dataset.push([timeTesz , entry.Year])
+			var specifier = '%M:%S'
 
+			data.forEach((entry) => {
+				var timeTesz = d3.timeParse(specifier)(entry.Time)
+				dataset.push([timeTesz , entry.Year])
 			})
+
+			var maxYear = d3.max(dataset, d => d[1])
+			var minYear = d3.min(dataset, d => d[1])
+
+			var xScale = d3
+				.scaleLinear()
+				.domain([minYear -2, maxYear + 2])
+				.range([0, w])
+
+			var yScale = d3
+				.scaleTime()
+				.domain(d3.extent(dataset, d => d[0]))
+				.range([20 , h ])
+
+			const xAxis = d3.axisBottom(xScale)
+			const yAxis = d3.axisLeft(yScale)
 
 			const svg = d3.select('body')
 				.append('svg')
@@ -20,12 +36,13 @@ $(document).ready(function() {
 				.attr('height', h)
 
 			svg.selectAll('circle')
-				.data(data)
+				.data(dataset)
 				.enter()
 				.append('circle')
-				.attr('cx', (d, i) => d.Year )
-				.attr('cy', (d, i) => h - console.log(dataset))
-				.attr('r', 5)
+				.attr('cx', d => xScale(d[1]))
+				.attr('cy', d => yScale(new Date (d[0])))
+				.attr('r', 7)
+				.attr('fill', '#4286f4')
 
 		}
 	)
